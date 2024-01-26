3b1a97bf1d55639bb43121d004bf6d235df445c2
Victor Piedra <victor.piedrab@ug.edu.ec>
2024-01-25 21:38:37 -0500

    feat(tasks): task created method POST

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from .models import Task
 
 def tasks(request):
-  return render(request, 'tasks/index.html')
+
+  if request.method == 'GET':
+    tasks = Task.objects.all()
+    
+
+    return render(request, 'tasks/index.html', {
+      'tasks': tasks,
+    })
+  
+  else:
+    title = request.POST.get('title')
+    Task.objects.create(title=title)
+
+    return redirect('index')
