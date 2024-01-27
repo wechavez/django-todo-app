31581cdc94ccde6bf4d1cd9158a3e979dbe1b574
Wilmer Chavez <wechavez14@gmail.com>
2024-01-26 19:27:11 -0500

    fix(tasks): routes definitions

--- tasks/forms/tasks.py
+++ tasks/forms/tasks.py
@@ -4,7 +4,6 @@ class CreateTask(forms.Form):
   title = forms.CharField(
     label='',
     widget=forms.TextInput(
-
       attrs={
         'placeholder': 'Escribe una tarea...',
         'class': 'p-2 rounded-lg w-full border border-slate-200'
--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.tasks, name='index'),
+    path('', views.tasks, name='index'),
+    path('complete/<int:id>', views.complete),
+    path('delete/<int:id>', views.delete),
 ]
